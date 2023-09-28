83f0d2e3a7d8f23cd55e3d0a77bfd809cdcba52c
DiegoAlb09 <diegoalb04hat@gmail.com>
2023-09-28 17:50:44 -0600

    Aumentar el arbol genealogico

--- Lenguajes_Inteligentes/Family_Tree.pl
+++ Lenguajes_Inteligentes/Family_Tree.pl
@@ -1,9 +1,17 @@
-female(pammi).
+female(pammi). 
 female(lizza).
 female(patty).
 female(anny).
 female(jean).
-female(anny).
+female(anabell).
+female(andrea).
+female(ximena).
+female(loise).
+female(martha).
+female(claudia).
+female(jesse).
+female(emily).
+female(stefany).
 
 male(jimmy).
 male(bobby).
@@ -11,19 +19,53 @@ male(tomy).
 male(pitter).
 male(john).
 male(johnson).
+male(alex).
+male(david).
+male(diego).
+male(francisco).
+male(malcom).
+male(scott).
+male(matt).
 
 parent(johnson,pammi).
 parent(anny,pammi).
-parent(pammi,bobby).
-parent(tomy,bobby).
-parent(tomy,lizza).
-parent(bobby,anny).
-parent(bobby,patty).
-parent(patty,jimmy).
-parent(bobby,pitter).
-parent(pitter,jean).
-parent(pitter,john).
+parent(johnson,alex).
+parent(anny,alex)
+parent(johnson,tomy).
+parent(anny,tomy).
 
+parent(pammi,lizza).
+parent(bobby,lizza).
+
+parent(alex,patty).
+parent(jean,patty).
+parent(alex,pitter).
+parent(jean,pitter).
+parent(alex,john).
+parent(jean,john).
+
+parent(tomy,andrea).
+parent(anabell,andrea).
+parent(tomy,ximena).
+parent(anabell,ximena).
+
+parent(ximena,loise).
+parent(david,loise).
+
+parent(diego,francisco).
+parent(lizza,francisco).
+
+parent(malcom,scott).
+parent(patty,scott).
+
+parent(scott,jesse).
+parent(martha,jesse).
+
+parent(francisco,emily).
+parent(claudia,emily).
+
+parent(matt,stefany).
+parent(loise,stefany).
 
 mother(X,Y):- parent(X,Y),female(X).
 father(X,Y):- parent(X,Y),male(X).
